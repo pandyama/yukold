e7c8f304ca8407c373e583a5fba7e6bb7dd1913e
Meet Pandya <pandyama2@gmail.com>
2023-02-25 17:26:18 -0700

    initial project files

--- App.tsx
+++ App.tsx
@@ -1,20 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react'
 
-export default function App() {
+import { StatusBar } from 'expo-status-bar'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  Platform,
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  SafeAreaView,
+  useWindowDimensions
+} from 'react-native'
+import CurrentWeather from './components/currentWeather/CurrentWeather'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+export default function App () {
+  const [fontsLoaded] = useFonts({
+    'Quicksand-Regular': require('./assets/fonts/Quicksand-Regular.ttf')
+  })
+
+  const windowHeight = useWindowDimensions().height
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <KeyboardAvoidingView
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={{
+        flex: 1
+      }}
+      enabled
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.select({
+        ios: 0,
+        android: 500
+      })}
+    >
+      {/* // <SafeAreaView style={{ flex: 1 }}> */}
+      <View
+        style={{
+          ...styles.container,
+          minHeight: Math.round(windowHeight) - 80
+        }}
+        onLayout={onLayoutRootView}
+      >
+        <CurrentWeather></CurrentWeather>
+
+        <View style={styles.search}>
+          <TextInput style={styles.textInput} placeholder='Search city' />
+        </View>
+        <StatusBar style='auto' />
+      </View>
+    </KeyboardAvoidingView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#449AFF',
+    // alignItems: 'flex-start',
+    justifyContent: 'flex-start'
   },
-});
+  textInput: {
+    backgroundColor: '#eff3f9',
+    borderRadius: 10,
+    padding: 10,
+    width: '40%',
+    textAlign: 'center',
+    fontFamily: 'Quicksand-Regular',
+    fontSize: 20
+  },
+  search: {
+    width: '100%',
+    height: '40%',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    padding: 10
+    // borderWidth: 2
+  }
+})

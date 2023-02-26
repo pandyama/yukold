fa173510b3b1a464ff5880f7a31634eb64509d73
Meet Pandya <pandyama2@gmail.com>
2023-02-26 14:31:46 -0700

    currentWeather component styling updated

--- components/currentWeather/CurrentWeatherStyle.tsx
+++ components/currentWeather/CurrentWeatherStyle.tsx
@@ -2,14 +2,14 @@ import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
   card: {
-    borderRadius: 5,
+    // borderRadius: 5,
     // boxShadow: '0 2px 30px rgba(black, 0.2)',
     backgroundColor: '#eff3f9',
-    height: '50%',
+    height: '60%',
     alignItems: 'center',
-    justifyContent: 'space-around',
-    borderWidth: 1,
-    margin: 5
+    justifyContent: 'space-around'
+    // borderWidth: 1
+    // margin: 5
   },
   currentTemp: { borderWidth: 1, borderRadius: 10, padding: 10 },
   weatherDescription: {

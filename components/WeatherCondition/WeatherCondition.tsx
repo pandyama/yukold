5df7b373c32306ebc5239e169550ba96b5a5c5c7
Meet Pandya <pandyama2@gmail.com>
2023-02-26 19:43:44 -0700

    condition component updated with api data

--- components/WeatherCondition/WeatherCondition.tsx
+++ components/WeatherCondition/WeatherCondition.tsx
@@ -2,28 +2,64 @@ import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View, Image } from 'react-native'
 import { styles } from './WeatherConditionStyle'
 
-export default function WeatherCondition () {
+interface Conditions {
+  feelsLike: string
+  wind: string
+  humidity: string
+  high: string
+  low: string
+}
+
+export default function WeatherCondition ({
+  feelsLike,
+  wind,
+  humidity,
+  high,
+  low
+}: Conditions) {
   return (
     <View>
       <View style={styles.row}>
         <View style={styles.city}>
-          <Text style={{ ...styles.text, fontSize: 25 }}>Feels Like</Text>
-          <Text style={{ ...styles.text, fontSize: 20 }}>42</Text>
+          <Text style={{ ...styles.text, fontSize: 27, color: 'black' }}>
+            Feels Like
+          </Text>
+          <Text style={{ ...styles.text, fontSize: 22 }}>
+            {feelsLike} &deg;C
+          </Text>
         </View>
         <View style={styles.city}>
-          <Text style={{ ...styles.text, fontSize: 25 }}>Wind</Text>
-          <Text style={{ ...styles.text, fontSize: 20 }}>24 km/h</Text>
+          <Text style={{ ...styles.text, fontSize: 27, color: 'black' }}>
+            Wind
+          </Text>
+          <Text style={{ ...styles.text, fontSize: 22 }}>{wind} km/h</Text>
         </View>
       </View>
       <View style={styles.row}>
         <View style={styles.city}>
-          <Text style={{ ...styles.text, fontSize: 25 }}>Precipitation</Text>
-          <Text style={{ ...styles.text, fontSize: 20 }}>70%</Text>
+          <Text style={{ ...styles.text, fontSize: 27, color: 'black' }}>
+            Day High
+          </Text>
+          <Text style={{ ...styles.text, fontSize: 22 }}>{high} &deg;C</Text>
         </View>
         <View style={styles.city}>
-          <Text style={{ ...styles.text, fontSize: 25 }}>Humidity</Text>
-          <Text style={{ ...styles.text, fontSize: 20 }}>95%</Text>
+          <Text style={{ ...styles.text, fontSize: 27, color: 'black' }}>
+            Day Low
+          </Text>
+          <Text style={{ ...styles.text, fontSize: 22 }}>{low} &deg;C</Text>
         </View>
+        {/* <View style={styles.city}>
+          <Text style={{ ...styles.text, fontSize: 27, color: 'black' }}>
+            Precipitation
+          </Text>
+          <Text style={{ ...styles.text, fontSize: 22 }}>70%</Text>
+        </View>
+        <View style={styles.city}>
+          <Text style={{ ...styles.text, fontSize: 27, color: 'black' }}>
+            Humidity
+          </Text>
+          <Text style={{ ...styles.text, fontSize: 22 }}>{humidity}%</Text>
+        </View> */}
       </View>
     </View>
   )
--- components/WeatherCondition/WeatherConditionStyle.tsx
+++ components/WeatherCondition/WeatherConditionStyle.tsx
@@ -3,29 +3,18 @@ import { StyleSheet } from 'react-native'
 export const styles = StyleSheet.create({
   row: {
     borderRadius: 5,
-    // boxShadow: '0 2px 30px rgba(black, 0.2)',
-    // backgroundColor: '#eff3f9',
-    // height: '50%',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-evenly',
-    // borderWidth: 1,
     marginTop: 20
   },
   city: {
-    // borderWidth: 1,
-    // borderRadius: 10,
-    // padding: 10,
-    // marginTop: 15
-    // position: 'absolute',
-    // top: 0,
-    // left: 0,
-    // width: '100%'
     alignItems: 'center',
-    justifyContent: 'space-evenly'
+    width: '50%',
+    justifyContent: 'space-around'
   },
   text: {
-    fontFamily: 'Quicksand-Regular',
+    fontFamily: 'RadioCanada-Regular',
     color: 'white'
   }
 })

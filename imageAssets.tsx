2458aa5a9fd124ace8857348a3f170218345026a
Meet Pandya <pandyama2@gmail.com>
2023-03-12 12:48:43 -0600

    image assets object updated with all the images

---
+++ imageAssets.tsx
@@ -0,0 +1,17 @@
+const imageAssets = {
+  heavyThunderstorm: require('./assets/weather/Thunderstorm/Heavy-Thunderstorm.png'),
+  lightThunderstorm: require('./assets/weather/Thunderstorm/Light-Thunderstorm.png'),
+  heavyRain: require('./assets/weather/Drizzle-Rain/Heavy-Rain.png'),
+  lightRain: require('./assets/weather/Drizzle-Rain/Light-Rain.png'),
+  mildRain: require('./assets/weather/Drizzle-Rain/Mild-Rain.png'),
+  rainy: require('./assets/weather/Drizzle-Rain/Rainy.png'),
+  clearSunny2: require('./assets/weather/Clear/Clear-Sunny2.png'),
+  clearSunny3: require('./assets/weather/Clear/Clear-Sunny3.png'),
+  clearSunny: require('./assets/weather/Clear/ClearSunny.png'),
+  heavySnow: require('./assets/weather/Snow/Heavy-Snow.png'),
+  lightSnow: require('./assets/weather/Snow/Light-Snow.png'),
+  lightSnow2: require('./assets/weather/Snow/Light-Snow2.png'),
+  mildSnow: require('./assets/weather/Snow/Mild-Snow.png')
+}
+
+export default imageAssets

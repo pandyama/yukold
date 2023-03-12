49584a2728fb752d645fb13db15b95be4dd04283
Meet Pandya <pandyama2@gmail.com>
2023-03-12 14:26:45 -0600

    handling weather codes and timezones

--- api/openweather/index.js
+++ api/openweather/index.js
@@ -2,6 +2,69 @@ import axios from "axios";
 import moment from "moment";
 import Constants from "expo-constants";
 
+const lightSnowCodes = [600, 612, 615, 620];
+const heavySnowCodes = [602, 622];
+const otherSnowCodes = [601, 611, 613, 616, , 621];
+
+const lightRainCodes = [500, 520, 531];
+const heavyRainCodes = [502, 503, 504, 522];
+const otherRainCodes = [501, 511, 521, 531];
+
+const lightThunderstormCodes = [200, 210, 231];
+const heavyThunderstormCodes = [202, 212, 232];
+const otherThunderstormCodes = [201, 211, 221, 230];
+
+const getRandomNumber = () => Math.floor(Math.random() * 2);
+
+const getWeatherIcon = (code) => {
+  if (lightThunderstormCodes.includes(code)) {
+    return "lightThunderstorm";
+  }
+  if (heavyThunderstormCodes.includes(code)) {
+    return "heavyThunderstorm";
+  }
+  if (otherThunderstormCodes.includes(code)) {
+    return "lightThunderstorm";
+  }
+
+  if (lightSnowCodes.includes(code)) {
+    return "lightSnow";
+  }
+  if (heavySnowCodes.includes(code)) {
+    return "heavySnow";
+  }
+  if (otherSnowCodes.includes(code)) {
+    const num = getRandomNumber();
+    if (num === 0) {
+      return "mildSnow";
+    }
+    return "lightSnow2";
+  }
+
+  if (lightRainCodes.includes(code)) {
+    return "mildRain";
+  }
+  if (heavyRainCodes.includes(code)) {
+    return "heavyRain";
+  }
+  if (otherRainCodes.includes(code)) {
+    return "lightRain";
+  }
+  if (code >= 300 && code < 322) {
+    return "rainy";
+  }
+
+  if (code >= 700 && code < 800) {
+    return "clearSunny2";
+  }
+  if (code === 800) {
+    return "clearSunny";
+  }
+  if (code > 800) {
+    return "clearSunny3";
+  }
+};
+
 export const weather = async (city, lat, lon) => {
   const key = Constants.expoConfig.extra.API_KEY;
 
@@ -28,14 +91,34 @@ export const weather = async (city, lat, lon) => {
     }
   }
 
-  const utc = moment();
-  const localTimeOffset = test.data.timezone / 3600;
-  const localTimeStamp = moment
-    .utc(utc)
-    .subtract(Math.abs(localTimeOffset), "hours");
+  const weatherCode = test.data.weather[0].id;
+  console.log(
+    "🚀 ~ file: index.js:95 ~ weather ~ test.data:",
+    test.data.timezone
+  );
+  const icon = getWeatherIcon(weatherCode);
 
-  const localDate = moment(localTimeStamp).format("MM-DD-YYYY");
-  const localTime = moment(localTimeStamp).format("hh:mm A");
+  const utc = moment();
+  let localTimeOffset;
+  let localTimeStamp;
+  let localDate;
+  let localTime;
+
+  if (test.data.timezone < 0) {
+    localTimeOffset = test.data.timezone / 3600;
+    localTimeStamp = moment
+      .utc(utc)
+      .subtract(Math.abs(localTimeOffset), "hours");
+
+    localDate = moment(localTimeStamp).format("MMMM DD");
+    localTime = moment(localTimeStamp).format("hh:mm A");
+  } else {
+    localTimeOffset = test.data.timezone / 3600;
+    localTimeStamp = moment.utc(utc).add(Math.abs(localTimeOffset), "hours");
+
+    localDate = moment(localTimeStamp).format("MMMM DD");
+    localTime = moment(localTimeStamp).format("hh:mm A");
+  }
 
   const weatherStat = {
     cityName: test.data.name,
@@ -50,6 +133,7 @@ export const weather = async (city, lat, lon) => {
     humidity: ~~test.data.main.humidity,
     time: localTime,
     date: localDate,
+    icon,
   };
   return weatherStat;
 };

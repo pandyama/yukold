6756a20279a4846e3755873a52c5e4bbd06d8b74
Meet Pandya <pandyama2@gmail.com>
2023-03-26 12:34:50 -0600

    folder renamed

---
+++ components/CurrentWeather/CurrentWeather.tsx
@@ -0,0 +1,45 @@
+import { Text, View, Image } from 'react-native'
+import { styles } from './CurrentWeatherStyle'
+import CurrentTime from '../CurrentTime/CurrentTime'
+import imageAssets from '../../imageAssets'
+
+interface Weather {
+  temperature: string
+  description: string
+  time: string
+  date: string
+  city: string
+  icon: string
+}
+
+type T = keyof typeof imageAssets
+
+export default function CurrentWeather ({
+  temperature,
+  description,
+  time,
+  date,
+  city,
+  icon
+}: Weather) {
+  return (
+    <View style={styles.card}>
+      <View style={styles.city}>
+        <Text style={styles.text}>{city}</Text>
+        <CurrentTime time={time} date={date}></CurrentTime>
+      </View>
+      <View style={styles.icon}>
+        <Image
+          source={imageAssets[icon as T]}
+          style={{ resizeMode: 'center', width: '100%', height: '100%' }}
+        />
+      </View>
+      <View style={styles.currentTemp}>
+        <Text style={styles.text}>{temperature} &deg;C</Text>
+      </View>
+      <View style={styles.weatherDescription}>
+        <Text style={styles.descText}>{description?.toLocaleUpperCase()}</Text>
+      </View>
+    </View>
+  )
+}
---
+++ components/CurrentWeather/CurrentWeatherStyle.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#eff3f9',
+    height: '61%',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  currentTemp: {},
+  weatherDescription: {},
+  city: {
+    padding: 7,
+    // margin: 25,
+    width: '100%'
+  },
+  text: {
+    fontSize: 40,
+    fontFamily: 'RadioCanada-Regular'
+  },
+  descText: {
+    fontSize: 30,
+    fontFamily: 'RadioCanada-Regular'
+  },
+  icon: { width: '25%', height: '25%' }
+})

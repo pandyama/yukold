021d7c544e3a5b318dc198c247c3a72f5f91e4a6
Meet Pandya <pandyama2@gmail.com>
2023-04-06 22:11:14 -0600

    config files updated after release

--- app.config.js
+++ app.config.js
@@ -3,5 +3,18 @@ require("dotenv").config();
 export default {
   extra: {
     API_KEY: process.env.API_KEY,
+    eas: {
+      projectId: "c90f4720-589c-4c49-98d6-244e7e77b2e7",
+    },
+  },
+  android: {
+    package: "com.mp.yukold",
+    versionCode: 3,
+    version: "1.0.2",
+    adaptiveIcon: {
+      foregroundImage: "./assets/appIcon.png",
+      backgroundColor: "#FFFFFF",
+    },
+    softwareKeyboardLayoutMode: "pan",
   },
 };
